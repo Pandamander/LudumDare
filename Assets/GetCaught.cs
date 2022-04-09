5dbcfcd2b187942d7051288e271ac9b6f8deeb8b
bromoco <brice.morrison@gmail.com>
2022-04-09 15:24:19 -0700

    Added caught UI and reset

---
+++ Assets/GetCaught.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCaught : MonoBehaviour
+{
+    [SerializeField] private OnOffUI endingUI;
+    private PlayerInputController input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<PlayerInputController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R) && input.canStillMove == false) // Reset if you've been caught
+        {
+            RestartGame();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "TheLaw") // Get caught
+        {
+            Debug.Log("Caught!");
+            endingUI.ShowUI();
+            input.canStillMove = false;
+        }
+    }
+
+    private void RestartGame()
+    {
+        input.ResetPlayer(); // Reset the player
+        endingUI.HideUI();
+    }
+}
--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 public class PlayerInputController : MonoBehaviour
 {
     public PlayerMovement playerMovement;
+    public bool canStillMove;
+    [SerializeField] private Transform startPosition;
 
     // Update is called once per frame
+
+    private void Start()
+    {
+        ResetPlayer();
+
+    }
+
+    public void ResetPlayer()
+    {
+        canStillMove = true;
+        gameObject.transform.position = startPosition.position;
+    }
+
     void Update()
     {
-        playerMovement.SetInput(
+        if (canStillMove) // Can't move if you've been caught
+        {
+            playerMovement.SetInput(
             new Vector2(
                 Input.GetAxis("Horizontal"),
                 Input.GetAxis("Vertical")
             )
         );
+        }
     }
 }
